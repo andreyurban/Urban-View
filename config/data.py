9df04f87d52de0095cac3628e8301734a01ad255
MisnikovQA <andriy_m@urban-digital.co.il>
2024-02-20 22:25:06 +0200

    with allure and new tests

--- config/data.py
+++ config/data.py
@@ -2,5 +2,7 @@ import os
 
 class Data:
 
+    WRONG_LOGIN = os.getenv("WRONG_LOGIN")
+    WRONG_PASSWORD = os.getenv("WRONG_PASSWORD")
     LOGIN = os.getenv("LOGIN")
     PASSWORD = os.getenv("PASSWORD")
