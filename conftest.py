a9b5289702a3ccda554c61c063c437e78ac31335
MisnikovQA <andriy_m@urban-digital.co.il>
2024-02-21 22:09:00 +0200

    22:51

--- conftest.py
+++ conftest.py
@@ -7,7 +7,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 def driver(request):
     options = Options()
     #options.add_argument("--headless")
-    options.add_argument("--no-sendbox")
+    options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
