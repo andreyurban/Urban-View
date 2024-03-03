3bd99b1d6c07c2b05a3ff61cda2b72887654cc1f
Andrey <125584460+andreyurban@users.noreply.github.com>
2024-03-03 21:58:44 +0200

    First CI test6

--- conftest.py
+++ conftest.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 def driver(request):
     options = Options()
     options.add_argument("--headless")
+    options.add_argument('--disable-gpu')
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
