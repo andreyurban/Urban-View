db6f4ac38f780bae5a5786cd43d0b21ebd81f2fb
Andrey <125584460+andreyurban@users.noreply.github.com>
2024-03-03 20:34:49 +0200

    First CI test1

--- conftest.py
+++ conftest.py
@@ -1,20 +1,18 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture(scope="function", autouse=True)
 def driver(request):
     options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
+    #options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
-    options.add_argument("--disable-software-rasterizer")
-
-    service = Service(executable_path='/usr/bin/chromedriver')
-    driver = webdriver.Chrome(service=service, options=options)
+    driver = webdriver.Chrome(options=options)
     request.cls.driver = driver
     yield driver
     driver.quit()
+
+
+
